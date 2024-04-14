1371e3d4a49e0c4d1645d4c65c8344ceaaffb3fe
jxnge <filipmarczyk@gmail.com>
2024-04-14 16:14:17 +0200

    first commit

---
+++ Budget-Buddy-logic/Budget.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Budget_Buddy_logic
+{
+    internal class Budget
+    {
+        private string _name;
+        private float _amount;
+
+        public Budget(string name, float amount)
+        {
+            _name = name;
+            _amount = amount;
+        }
+
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+
+        public float Amount
+        {
+            get { return _amount; }
+            set { _amount = value; }
+        }
+
+        public void Display()
+        {
+            Console.WriteLine($"Witaj {_name}! Twój aktualny budżet wynosi {_amount} zł.");
+        }   
+    }
+}
---
+++ Budget-Buddy-logic/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Budget_Buddy_logic
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.Write("Podaj swoje imię: ");
+            string name = Console.ReadLine();
+            Console.Write("Podaj swój aktualny budżet (format 0,00): ");
+            float initialBudget = float.Parse(Console.ReadLine());
+            Budget budget = new Budget(name, initialBudget);
+            budget.Display();
+        }
+    }
+}
