72c181023405641e8b2fc12454fa41061b6cf89f
jxnge <filipmarczyk@gmail.com>
2024-06-22 14:23:52 +0200

    diagram

--- budget-buddy/budget-buddy-winforms/obj/Debug/net8.0-windows/budget-buddy.AssemblyInfo.cs
+++ budget-buddy/budget-buddy-winforms/obj/Debug/net8.0-windows/budget-buddy.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("budget-buddy")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+24820e6573d4f69c662dab651face83cc6ae57e1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+335eee7585166ea1c3ed5416aff50110296d1164")]
 [assembly: System.Reflection.AssemblyProductAttribute("budget-buddy")]
 [assembly: System.Reflection.AssemblyTitleAttribute("budget-buddy")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
